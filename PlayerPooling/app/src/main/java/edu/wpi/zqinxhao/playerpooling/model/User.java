5bad64aca56350536759b0d08e7756e5a86c4f56
XJ HAO <xhao@XJs-MacBook-Pro.local>
2016-04-20 19:46:34 -0400

    implemented User

--- PlayerPooling/app/src/main/java/edu/wpi/zqinxhao/playerpooling/model/User.java
+++ PlayerPooling/app/src/main/java/edu/wpi/zqinxhao/playerpooling/model/User.java
@@ -2,4 +2,40 @@ package edu.wpi.zqinxhao.playerpooling.model;
 
 
 public class User {
+    private String name;
+    private String email;
+    private String hashPassword;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHashPassword() {
+        return hashPassword;
+    }
+
+    public void setHashPassword(String hashPassword) {
+        this.hashPassword = hashPassword;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
