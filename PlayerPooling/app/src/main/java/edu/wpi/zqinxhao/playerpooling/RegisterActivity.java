d2c902155fcbb99dd1141760ec9354095f686ed5
XJ HAO <xhao@XJs-MacBook-Pro.local>
2016-04-19 23:04:51 -0400

    login UI

---
+++ PlayerPooling/app/src/main/java/edu/wpi/zqinxhao/playerpooling/RegisterActivity.java
@@ -0,0 +1,23 @@
+package edu.wpi.zqinxhao.playerpooling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        final EditText etName = (EditText) findViewById(R.id.etName);
+        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final EditText etConfirmPassword = (EditText) findViewById(R.id.etPasswordConfirm);
+        final EditText etAge = (EditText) findViewById(R.id.etAge);
+        final Button bRegister = (Button) findViewById(R.id.bRegister);
+
+    }
+}
