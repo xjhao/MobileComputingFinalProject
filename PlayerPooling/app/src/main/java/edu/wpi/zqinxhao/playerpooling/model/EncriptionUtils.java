9d9766919bb78820ebb0cb62e702968246c3d36b
susansan77 <susansan77@gmail.com>
2016-04-20 19:42:09 -0400

    move the encription to a standalone java file

---
+++ PlayerPooling/app/src/main/java/edu/wpi/zqinxhao/playerpooling/model/EncriptionUtils.java
@@ -0,0 +1,55 @@
+package edu.wpi.zqinxhao.playerpooling.model;
+
+import android.util.Base64;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by zishanqin on 4/20/16.
+ */
+   public class EncriptionUtils {
+    private static String convertToHex(byte[] data) throws java.io.IOException {
+        StringBuffer sb = new StringBuffer();
+        String hex=null;
+
+        hex= Base64.encodeToString(data, 0, data.length, Base64.NO_PADDING);
+
+        sb.append(hex);
+
+        return sb.toString();
+    }
+
+
+    public static String computeSHAHash(String password) {
+        MessageDigest mdSha1 = null;
+        String SHAHash = null;
+
+        try {
+            mdSha1 = MessageDigest.getInstance("SHA-1");
+        } catch (NoSuchAlgorithmException e1) {
+            Log.e("myapp", "Error initializing SHA1 message digest");
+        }
+
+        try {
+            mdSha1.update(password.getBytes("ASCII"));
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        byte[] data = mdSha1.digest();
+
+        try {
+            SHAHash = convertToHex(data);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return SHAHash;
+    }
+}
