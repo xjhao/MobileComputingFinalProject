a5e5cf502d9de708de9eae280ec3cfa6e9e8775a
XJ HAO <xhao@XJs-MacBook-Pro.local>
2016-04-22 23:35:22 -0400

    UI design of create game

--- PlayerPooling/app/src/main/java/edu/wpi/zqinxhao/playerpooling/CreateGameActivity.java
+++ PlayerPooling/app/src/main/java/edu/wpi/zqinxhao/playerpooling/CreateGameActivity.java
@@ -2,6 +2,7 @@ package edu.wpi.zqinxhao.playerpooling;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class CreateGameActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class CreateGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_game);
+
     }
 }
